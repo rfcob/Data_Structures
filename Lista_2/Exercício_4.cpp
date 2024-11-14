c2bf3d0c328f77f501a063bd49256ee89510402f
RFcobo <rafaelcobo@outlook.com>
2024-11-14 15:53:06 -0300

    Exercicio determinante fim - 14/11/2024

--- Lista_2/Exercício_4.cpp
+++ Lista_2/Exercício_4.cpp
@@ -2,9 +2,36 @@
 #include <iostream>
 using namespace std;
 
+
+//Imprimindo uma matriz
+
+void imprime(int matriz[3][3]){
+    cout << "A matriz que entrou é: " <<endl <<"\n";
+    for(int i=0; i <3; i++){
+        for(int j=0; j <3; j++){
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+// Calculando o determinante
+int determinante(int matriz[3][3]){
+    int det;
+
+    det = matriz[0][0] * (matriz[1][1] * matriz[2][2] - matriz[1][2] * matriz[2][1]) 
+        - matriz[0][1] * (matriz[1][0] * matriz[2][2] - matriz[1][2] * matriz[2][0]) 
+        + matriz[0][2] * (matriz[1][0] * matriz[2][1] - matriz[1][1] * matriz[2][0]);
+
+    //cout << det << endl;
+
+   return det;
+}
+
 int main(){
 
-    int matriz[3][3], matriz_transp[3][3], i , j;
+    int matriz[3][3], i , j, det;
 
     //Criando a matriz
     cout << "Entre com os valores da matriz 3x3: " << endl;
@@ -13,6 +40,12 @@ int main(){
             cin >> matriz[i][j];
         }
     }
+
+    imprime(matriz);
+
+    cout << endl;
+
+    cout <<"O determinante da matriz é: " << (det = determinante(matriz)) << endl << "\n";
 
     cout << endl;
 
