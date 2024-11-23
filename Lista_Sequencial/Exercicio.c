963ad95ff94ebd009ab69063f3b468803421faea
RFcobo <rafaelcobo@outlook.com>
2024-11-23 19:00:34 -0300

    meio do caminho - faltam coisas

--- Lista_Sequencial/Exercicio.c
+++ Lista_Sequencial/Exercicio.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ListaOrdenada.h"
 #include "ListaOrdenada.c"
 
-int resize_array(LISTA* lista);
+int resize_array(LISTA* lista, int tamanho_lista){
+
+    int tl=sizeof(LISTA);
+
+    if(tamanho_lista<= tl/2){
+        return tamanho_lista;
+    }else{
+        lista = (LISTA *) realloc(lista,2* sizeof(LISTA));
+    }
+
+}//precisa-se testar quantos elementos tem na lista e quantos cabem ainda. Assim, podemos criar condicionais ou usar um contador que parte do valor da lista. quando 
+//chegar a zero, resize a lista 
 
 int main(){
 
-    LISTA lista;
+    int tamanho_lista;
+    //int *tam=tamanho_lista;
+    LISTA *lista = (LISTA *) malloc(sizeof(LISTA)); //inicializando a Lista dinamicamente.
     REGISTRO reg;
 
 
-    int tamanho_lista=3, i=0;
-    inicializarLista(&lista);
+    inicializarLista(lista);
 
-    for(i; i<tamanho_lista;i++){
+    printf("Digite a quantidade de itens a inserir:\n");
+    scanf("%d", &tamanho_lista);
+    
+    resize_array(lista, tamanho_lista);
+
+    for(int i=0; i<tamanho_lista;i++){
+                printf("Digite o valor: ", i);
         scanf("%d", &reg.chave);
-        inserirElemListaOrd(&lista, reg);
+        inserirElemListaOrd(lista, reg);
     }
 
-    exibirLista(&lista);
+    exibirLista(lista);
+    printf("Numero de elementos na lista: %i.\n", tamanho(lista));
+    printf("Tamanho da lista (em bytes): %i.\n", (sizeof(LISTA) / sizeof(REGISTRO))-2);
 
 
     return 0;
