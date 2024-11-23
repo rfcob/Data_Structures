368e668a43aa8d43a2ef778bf7c3bdbac96cc6e8
RFcobo <rafaelcobo@outlook.com>
2024-11-23 17:06:34 -0300

    novo cliente

--- Lista_Sequencial/Exercicio.c
+++ Lista_Sequencial/Exercicio.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include "ListaOrdenada.h"
+#include "ListaOrdenada.c"
 
+int resize_array(LISTA* lista);
+
+int main(){
+
+    LISTA lista;
+    REGISTRO reg;
+
+
+    int tamanho_lista=3, i=0;
+    inicializarLista(&lista);
+
+    for(i; i<tamanho_lista;i++){
+        scanf("%d", &reg.chave);
+        inserirElemListaOrd(&lista, reg);
+    }
+
+    exibirLista(&lista);
+
+
+    return 0;
+}
